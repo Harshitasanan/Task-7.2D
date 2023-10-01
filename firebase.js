32a2ef643823265e4de05227c7cfbc0840f378a1
Unique_h <132455967+Harshitasanan@users.noreply.github.com>
2023-10-01 16:01:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyp1vjkHob7t9xFMAYIkKs6i-o_ScHLdk",
-  authDomain: "cardstorage-44338.firebaseapp.com",
-  projectId: "cardstorage-44338",
-  storageBucket: "cardstorage-44338.appspot.com",
-  messagingSenderId: "974835535904",
-  appId: "1:974835535904:web:ce04eba3d11340fd012c45"
+  apiKey: "AIza*********",
+  authDomain: "cardst************",
+  projectId: "cardst***********",
+  storageBucket: "cardst*********",
+  messagingSenderId: "**********",
+  appId: "1:974835535904:web:ce************"
 };
 
 // Initialize Firebase
